ea53826662aebc0c090c8271d7ff68ae34369ac1
Hridoy Hossain <ridoy.java@gmail.com>
2021-11-08 22:47:37 +0600

    find all the id in oncreate method

--- app/src/main/java/com/example/firebaeimagerecycerview/MainActivity.java
+++ app/src/main/java/com/example/firebaeimagerecycerview/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.firebaeimagerecycerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    Button choseImage,saveImage,showImge;
+    EditText imageName;
+    ProgressBar progressBar;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        choseImage = findViewById(R.id.chosePhotoButtoId);
+        saveImage = findViewById(R.id.saveImageButtonId);
+        showImge = findViewById(R.id.showAllImageButtonId);
+        imageName=findViewById(R.id.choseImageNameTextViewId);
+        progressBar = findViewById(R.id.progressbarId);
+        imageView=findViewById(R.id.choseImageViewId);
+
     }
 }
